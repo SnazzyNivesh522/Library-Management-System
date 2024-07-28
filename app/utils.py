1d79c35300fa46d1fc91396bb91312b88089b8f0
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-07-28 11:51:46 +0530

    added the functionality of getting book details through ISBN from Google Books API

--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,7 @@ from flask_login import current_user
 from flask import current_app
 from flask_mail import Message
 from app import mail
+import requests
 
 from PIL import Image
 
@@ -44,3 +45,21 @@ def save_picture(form_picture):
     i.save(picture_path)
 
     return picture_fn
+def fetch_book_details_from_api(isbn):
+    url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if 'items' in data and len(data['items']) > 0:
+            volume_info = data['items'][0]['volumeInfo']
+            return {
+                'isbn': isbn,
+                'title': volume_info.get('title', 'Unknown'),
+                'author': ', '.join(volume_info.get('authors', ['Unknown'])),
+                'publisher': volume_info.get('publisher', 'Unknown'),
+                'year': volume_info.get('publishedDate', 'Unknown')[:4],
+                'categories': ', '.join(volume_info.get('categories', ['Unknown'])),
+                'description': volume_info.get('description', ''),
+                'image_link': volume_info.get('imageLinks', {}).get('thumbnail', '')
+            }
+    return None
